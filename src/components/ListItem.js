390d8caa7ac3bde1181b6a7f178932d30ba7803b
PARAVPREET17 <psingh9_be20@thapar.edu>
2023-06-24 17:54:07 +0530

    hello

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const ListItem = ({note}) => { // Props are immutable when pass down from parent to child(parameters)
+const ListItem = ({ note }) => {
+  // Props are immutable when pass down from parent to child(parameters)
 
   return (
     <Link to={`/note/${note.id}`}>
-      <h3>{note.body}</h3>
+      <div>
+        <h3>{note.body}</h3>
+      </div>
     </Link>
-  )
-}
+  );
+};
 
-export default ListItem
+export default ListItem;
